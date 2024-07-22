a2b39bc95593798bc8c08cc2babb38baae01fd08
Jhon Ospina <jhonospina2711@gmail.com>
2024-07-21 21:35:26 -0500

    Seccion 5 Clase Modulo DBZ

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainPageComponent } from './pages/main-page.component';
+
+
+
+@NgModule({
+  exports: [
+    MainPageComponent
+  ],
+  declarations: [
+    MainPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DbzModule { }
